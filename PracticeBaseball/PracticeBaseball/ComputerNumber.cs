9bee67aca5312934674d91755f9c86e9636fe1a9
k k <sh0226sh@naver.com>
2018-10-31 21:29:26 +0900

    18-10-31 오후 9:29 김소현 1. UserInputNumber 메서드 추가 2. 컴퓨터번호와 유저번호의 비교문 추가 3. Ball, Strike, Out 판정 추가 4. 컴퓨터 번호 출력 제거

--- PracticeBaseball/PracticeBaseball/ComputerNumber.cs
+++ PracticeBaseball/PracticeBaseball/ComputerNumber.cs
@@ -11,7 +11,7 @@ namespace PracticeBaseball
         private Random _random = new Random();
 
         // 컴퓨터가 가진 숫자 알려줌   // 콘솔 프린트는 삭제예정
-        public void GetComputerNumber()
+        public List<int> GetComputerNumber(int number=3)
         {
             List<int> comList = new List<int>();
             bool isNum3 = false;
@@ -19,23 +19,29 @@ namespace PracticeBaseball
             // 컴퓨터 랜덤숫자 3개 출력
             while (!isNum3)
             {
-                returnRandomComList(comList);
+                ReturnRandomComList(comList);
 
-                if (comList.Count == 3)
+                if (comList.Count == number)
                     isNum3 = true;
             }
-            IEnumerable<int> comNum = from x in comList orderby x select x;
-            foreach (int com in comList)
-                Console.WriteLine(com);
+
+            // 테스트용. 삭제 예정
+            //foreach (int com in comList)
+            //        Console.WriteLine(com);
+
+            return comList.ToList();
         }
 
         // 컴퓨터의 랜덤번호 3개 선정
-        public void returnRandomComList(List<int> comList)
+        public void ReturnRandomComList(List<int> comList)
         {
-            int randNum = _random.Next(0, 9);
+            int count = 0;
+            int minNum = 0;
+            int maxNum = 9;
+            int randNum = _random.Next(minNum, maxNum);
             int saveNum = randNum;
 
-            if (comList.Count == 0)
+            if (comList.Count == count)
                 comList.Add(saveNum);
 
             else
@@ -44,7 +50,7 @@ namespace PracticeBaseball
 
                 while (IsSame)  // 중복인 경우
                 {
-                    randNum = _random.Next(0, 9);
+                    randNum = _random.Next(minNum, maxNum);
 
                     if (saveNum != randNum)
                         IsSame = false;
@@ -53,5 +59,5 @@ namespace PracticeBaseball
                 comList.Add(randNum);
             }
         }
-        }
+    }
 }
--- PracticeBaseball/PracticeBaseball/Program.cs
+++ PracticeBaseball/PracticeBaseball/Program.cs
@@ -13,18 +13,84 @@ namespace PracticeBaseball
         static void Main(string[] args)
         {
             ComputerNumber computer = new ComputerNumber();
+            List<int> randomNumber = new List<int>();
+            List<int> userNumber = new List<int>();
             // 컴퓨터의 번호 3개 출력
-            computer.GetComputerNumber();
+            randomNumber = computer.GetComputerNumber().ToList();
 
             // 플레이어가 정한 번호 3개 출력
+            InputUserNumber(userNumber);
 
             // 컴퓨터 번호와 플레이어 번호를 비교
-            
+            Result(randomNumber, userNumber);
 
-            // 모두 같거나 모두 다르지 않다는 조건 하에  1< 숫자 비교결과 <=2  같으면 Ball, 다르면 Strike
-            // 모두 같으면 3 Ball
-            // 모두 다르면 3 Out ( 숫자 0-9 목록에서 해당 숫자 제거 )
 
+        }
+
+        /// <summary>
+        /// 컴퓨터 번호와 플레이어 번호를 비교한 결과를 판정한다. 
+        /// 서로 번호가 일치하면 Ball, 일부만 틀리면 Strike, 전부 다 틀리면 Out 
+        /// </summary>
+        /// <param name="randomNumber"></param>
+        /// <param name="userNumber"></param>
+        private static void Result(List<int> randomNumber, List<int> userNumber)
+        {
+            //int totalCount = 3;
+            List<int> allNumber = new List<int>();
+            int idx = 0;
+            int countZero = 0;
+            int BallCount = 0;
+            int StrikeCount = 0;
+            int OutCount = 0;
+
+            // 0부터 9까지의 숫자 리스트
+            while (idx < 10)
+            {
+                allNumber.Add(idx);
+                idx++;
+            }
+            // 모두 같으면 BallCount = 3
+            // 모두 다르면 OutCount = 3
+            // 일부가 같으면 BallCount++, 일부가 다르면 StrikeCount++
+            // OutCount인 경우 숫자 0-9 목록에서 해당 숫자 제거
+            while (allNumber.Count > 3)
+            {
+                foreach (var com in randomNumber)
+                {
+                    bool isSameNumber = userNumber.Contains(com);
+
+                    if (isSameNumber)
+                        BallCount++;
+
+                    else if (BallCount != countZero && !isSameNumber)
+                        StrikeCount++;
+
+                    else if (BallCount == countZero && StrikeCount == countZero)
+                    {
+                        OutCount++;
+                        //allNumber.Contains()
+                    }
+                }
+
+                Console.WriteLine($"Ball: {BallCount} \nStrike: {StrikeCount} \nOut:{OutCount}");
+            }
+        }
+
+        private static void InputUserNumber(List<int> userNumber)
+        {
+            while (userNumber.Count < 3)
+            {
+                Console.WriteLine("유저의 숫자 입력");
+
+                string inputString = Console.ReadLine();
+                int saveUserNum = int.Parse(inputString);
+
+                if (userNumber.Count < 3 && !userNumber.Contains(saveUserNum))
+                    userNumber.Add(saveUserNum);
+
+                else if (userNumber.Contains(saveUserNum))
+                    Console.WriteLine("이미 있는 번호입니다. 다시 입력해 주세요.");
+            }
         }
     }
 }
